64e4825e6dfe19025f38c5ea5df138af41349af5
Bartosz Olczyk <bartez378@gmail.com>
2021-10-30 16:01:30 +0200

    added @Value

--- src/main/java/com/example/cqenginedemo/Course.java
+++ src/main/java/com/example/cqenginedemo/Course.java
@@ -1,29 +1,16 @@
 package com.example.cqenginedemo;
 
 import com.googlecode.cqengine.attribute.Attribute;
+import lombok.Value;
 
 import static com.googlecode.cqengine.query.QueryFactory.attribute;
 
+@Value
 public class Course {
 
-    private String name;
-    private String description;
+    String name;
+    String description;
 
     public static final Attribute<Course, String> COURSE_NAME = attribute("name", Course::getName);
     public static final Attribute<Course, String> COURSE_DESCRIPTION = attribute("description", Course::getDescription);
-
-    public Course(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-
 }
